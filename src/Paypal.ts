fd6bbff9bdccd95bc52f324b37cd77dde18e6531
Nicolas Godefroy <nicolas.godefroy.dev@gmail.com>
2022-08-03 09:51:01 +0200

    fix: log linking issues

--- src/Paypal.ts
+++ src/Paypal.ts
@@ -6,30 +6,26 @@ import type {
 } from "./types"
 
 const LINKING_ERROR =
-  `The package 'react-native-paypal' doesn't seem to be linked. Make sure: \n\n` +
+  `The package '@bounceapp/react-native-paypal' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: "" }) +
   "- You rebuilt the app after installing the package\n" +
   "- You are not using Expo managed workflow\n"
 
-const Paypal = NativeModules.Paypal
-  ? NativeModules.Paypal
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR)
-        },
-      },
-    )
+const logger = (message: string, ...rest: unknown[]) =>
+  console.info(`[@bounceapp/react-native-paypal] ${message}`, ...rest)
 
-const DEFAULT_OPTIONS = {
-  billingAgreementDescription: "",
-  shippingAddressRequired: false,
-}
+const isLinked = !!NativeModules.Paypal
+if (!isLinked) logger(LINKING_ERROR)
+
+const Paypal = isLinked ? NativeModules.Paypal : {}
 
 export const requestBillingAgreement = (
   options: RequestBillingAgreementOptions,
 ): Promise<RequestBillingAgreementResponse> => {
+  const DEFAULT_OPTIONS = {
+    billingAgreementDescription: "",
+    shippingAddressRequired: false,
+  }
   const composedOptions = { ...DEFAULT_OPTIONS, ...options }
 
   return Paypal.requestBillingAgreement(composedOptions)
