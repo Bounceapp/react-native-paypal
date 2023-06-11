20e2a43754efb02509e9da7653d82fdf36405c8e
Nicolas Godefroy <nicolas.godefroy.dev@gmail.com>
2023-06-11 11:35:20 +0200

    feat: react native 0.71.8

--- src/Paypal.ts
+++ src/Paypal.ts
@@ -6,7 +6,7 @@ import type {
 } from "./types"
 
 const LINKING_ERROR =
-  `The package '@bounceapp/react-native-paypal' doesn't seem to be linked. Make sure: \n\n` +
+  `[@bounceapp/react-native-paypal]: the package doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: "" }) +
   "- You rebuilt the app after installing the package\n" +
   "- You are not using Expo managed workflow\n"
